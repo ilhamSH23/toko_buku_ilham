4c2d65e7959f39fbbea5c23e35fece186b7c1cdd
ilhamSH23 <blajar50@gmail.com>
2024-05-27 12:32:48 +0700

    DIGIDAW

--- index.php
+++ index.php
@@ -5,57 +5,58 @@ require_once 'layout/header.php';
 
 
 
-  <aside id="sidebar" class="sidebar">
+<aside id="sidebar" class="sidebar">
 
-    <ul class="sidebar-nav" id="sidebar-nav">
+  <ul class="sidebar-nav" id="sidebar-nav">
 
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="index.html">
-          <i class="bi bi-grid"></i>
-          <span>Dashboard</span>
-        </a>
-    
-      </li>End Icons Nav 
+    <li class="nav-item">
+      <a class="nav-link collapsed" href="index.html">
+        <i class="bi bi-grid"></i>
+        <span>Dashboard</span>
+      </a>
 
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="/page/buku/buku1.php">
+    </li>End Icons Nav
 
-          <span>Buku</span>
-        </a>
-        </li>
+    <li class="nav-item">
+      <a class="nav-link collapsed" href="/page/buku/buku1.php">
+        <i class="bi bi-book"></i>
+        <span>Buku</span>
+      </a>
+    </li>
 
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="/page/buku/penjualan.php">
-          <span>Penjualan</span>
-        </a>
-        </li>
+    <li class="nav-item">
+      <a class="nav-link collapsed" href="/page/buku/penjualan.php">
+        <i class="bi bi-bag-dash"></i>
+        <span>Penjualan</span>
+      </a>
+    </li>
 
-       <li class="nav-item">
-        <a class="nav-link collapsed" href="page/auth/login.php">
-          <i class="bi bi-box-arrow-in-right"></i>
-          <span>Login</span>
-        </a>
-        </li>
+    <li class="nav-item">
+      <a class="nav-link collapsed" href="page/auth/exit.php">
+        <i class="bi bi-box-arrow-in-right"></i>
+        <span>Login</span>
+      </a>
+    </li>
 
-      
 
-      <div class="row">
-        <div class="col-lg-6">
 
-        
+    <div class="row">
+      <div class="col-lg-6">
 
-        </div>
 
-        <div class="col-lg-6">
 
-          
-
-        </div>
       </div>
+
+      <div class="col-lg-6">
+
+
+
+      </div>
+    </div>
     </section>
 
-  </main>
+    </main>
 
-  <?php
-require_once 'layout/footer.php';
-?>
+    <?php
+    require_once 'layout/footer.php';
+    ?>
--- layout/sidebar.php
+++ layout/sidebar.php
@@ -1,26 +1,27 @@
 <aside id="sidebar" class="sidebar">
 
-<ul class="sidebar-nav" id="sidebar-nav">
+  <ul class="sidebar-nav" id="sidebar-nav">
 
-  <li class="nav-item">
-    <a class="nav-link collapsed" href="index.html">
-      <i class="bi bi-grid"></i>
-      <span>Dashboard</span>
-    </a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link collapsed" href="index.html">
-      <span>Buku</span>
-    </a>
-  </li>
-  <li class="nav-item">
-        <a class="nav-link collapsed" href="page/auth/login.php">
-          <i class="bi bi-box-arrow-in-right"></i>
-          <span>Login</span>
-        </a>  
-        </li>
+    <li class="nav-item">
+      <a class="nav-link collapsed" href="index.html">
+        <i class="bi bi-grid"></i>
+        <span>Dashboard</span>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link collapsed" href="index.html">
+        <i class="bi bi-book"></i>
+        <span>Buku</span>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link collapsed" href="page/auth/login.php">
+        <i class="bi bi-box-arrow-in-right"></i>
+        <span>Login</span>
+      </a>
+    </li>
 
- 
-</ul>
 
-</aside><!-- End Sidebar-->
+  </ul>
+
+</aside>
